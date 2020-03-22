51d8b0d5718c359a4e6347670181083e38793036
wangyufan <183509578@qq.com>
2020-03-22 11:11:03 +0800

    用户界面 - 商品筛选、排序

--- app/Http/Controllers/ProductsController.php
+++ app/Http/Controllers/ProductsController.php
@@ -2,15 +2,39 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\Request;
 use App\Models\Product;
-use Illuminate\Http\Request;
 
 class ProductsController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
-        $products = Product::where('on_sale', true)->paginate(8);
+        $key = $request->input('search', '');
+        $order = $request->input('order', '');
+        $productBuilder = Product::query()->where('on_sale', true);
 
-        return view('product.index', ['products' => $products]);
+        if ($key) {
+            $search = '%' . $key . '%';
+            $productBuilder->where('title', 'like', $search);
+
+            //关联模型包含
+            $productBuilder->orWhereHas('skus', function ($query) use ($search) {
+                $query->where('title', 'like', $search);
+            });
+        }
+
+        if ($order && (preg_match('/^(.+)_(asc|desc)$/', $order, $m)) && in_array($m[1], ['price', 'sold_count', 'rating'])) {
+            $productBuilder->orderBy($m[1], $m[2]);
+        }
+
+        $products = $productBuilder->paginate(4);
+
+        return view('product.index', [
+            'products' => $products,
+            'filters' => [
+                'search' => $key,
+                'order'  => $order
+            ]
+        ]);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,9 @@
 |
 */
 
+$clientId = 3;
+$clientSecret = '8ACEOdOjfKpma8j6M4shQif9Ak1L8FWNrGePzy11';
+
 Route::redirect('/', '/products')->name('root');
 Route::get('products', 'ProductsController@index')->name('products.index');
 
@@ -25,4 +28,81 @@ Route::group([
     Route::get('user_addresses/{user_address}', 'UserAddressesController@edit')->name('user_addresses.edit');
     Route::put('user_addresses/{user_address}', 'UserAddressesController@update')->name('user_addresses.update');
     Route::delete('user_addresses/{user_address}', 'UserAddressesController@destroy')->name('user_addresses.destroy');
+});
+
+//测试oauth
+// 第三方登陆，重定向
+Route::get('/apidemo/login',
+    function (\Illuminate\Http\Request $request) use ($clientId) {
+        $request->session()->put('state', $state = Str::random(40));
+
+        $query = http_build_query([
+            'client_id' => $clientId,
+            'redirect_uri' => 'http://shop.test/auth/callback',
+            'response_type' => 'code',
+            'scope' => '*',
+            'state' => $state,
+        ]);
+
+        return redirect('http://apidemo.test/oauth/authorize?'.$query);
+    });
+
+// 回调地址，获取 code，并随后发出获取 token 请求
+Route::view('/auth/callback', 'auth_callback');
+
+Route::post('/get/token', function (\Illuminate\Http\Request $request) use (
+    $clientId,
+    $clientSecret
+) {
+    // csrf 攻击处理
+    $state = $request->session()->pull('state');
+    throw_unless(
+        strlen($state) > 0 && $state === $request->params['state'],
+        InvalidArgumentException::class
+    );
+
+
+    $response
+        = (new \GuzzleHttp\Client())->post('http://apidemo.test/oauth/token', [
+        'form_params' => [
+            'grant_type' => 'authorization_code',
+            'client_id' => $clientId,
+            'client_secret' => $clientSecret,
+            'redirect_uri' => 'http://shop.test/auth/callback',
+            'code' => $request->params['code'],
+        ],
+    ]);
+
+    return json_decode((string)$response->getBody(), true);
+});
+// 刷新 token
+Route::view('/refresh/page', 'refresh_page');
+Route::post('/refresh', function (\Illuminate\Http\Request $request) use (
+    $clientId,
+    $clientSecret
+) {
+    $http = new GuzzleHttp\Client;
+    $response = $http->post('http://apidemo.test/oauth/token', [
+        'form_params' => [
+            'grant_type' => 'refresh_token',
+            'refresh_token' => $request->params['refresh_token'],
+            'client_id' => $clientId,
+            'client_secret' => $clientSecret,
+        ],
+    ]);
+
+    return json_decode((string)$response->getBody(), true);
+});
+
+Route::get('/yinshi', function () use(
+    $clientId
+){
+    $query = http_build_query([
+        'client_id' => $clientId,
+        'redirect_uri' => 'http://shop.test/auth/callback',
+        'response_type' => 'token',
+        'scope' => '',
+    ]);
+
+    return redirect('http://apidemo.test/oauth/authorize?'.$query);
 });
